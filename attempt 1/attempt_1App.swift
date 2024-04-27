dceedeaead2deef45ebb9eb507082d52b1898b40
mxrodriguez04 <103071498+mxrodriguez04@users.noreply.github.com>
2024-04-26 17:29:52 -0700

    Initial Commit

---
+++ attempt 1/attempt_1App.swift
@@ -0,0 +1,17 @@
+//
+//  attempt_1App.swift
+//  attempt 1
+//
+//  Created by Manuel Rodriguez on 4/26/24.
+//
+
+import SwiftUI
+
+@main
+struct attempt_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
